e0268762b98719da8b4131a5f4cd765fda15010a
unknown <alejandrojzeballos@gmail.com>
2022-03-09 20:45:31 -0400

    {add} inciando el proyecto reacthooks con react

--- apuntes.js
+++ apuntes.js
@@ -1,3 +1,10 @@
 /* 
+resuelven el componente hell
+las clases trabajan directamente con funciones y no con clases
+
+se puede usar metodos especiales sin la necesidad de compartir o enviar entre componentes
+
+
+
 
 */
