51c64d79382c3399ba5516fc2f8674feb37152ec
unknown <alejandrojzeballos@gmail.com>
2022-03-11 20:33:57 -0400

    {add} añadiendo el componente header con usestate

--- apuntes.js
+++ apuntes.js
@@ -5,6 +5,7 @@ las clases trabajan directamente con funciones y no con clases
 se puede usar metodos especiales sin la necesidad de compartir o enviar entre componentes
 
 
+----------USE STATE---------------------
 
 
 */
