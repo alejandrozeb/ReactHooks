51668246165ba34667ce7371cc2354a44cc84ba5
unknown <alejandrojzeballos@gmail.com>
2022-03-14 19:38:43 -0400

    {add} añadiendo el comopnente Character

--- apuntes.js
+++ apuntes.js
@@ -6,6 +6,14 @@ se puede usar metodos especiales sin la necesidad de compartir o enviar entre co
 
 
 ----------USE STATE---------------------
+react{useState}
 
+se instancion como un arrray
+[name, setname] = useState(False) valor por defecto
+
+--------------USE Effect------------
+nos olvidamos del ciclo de vida,y pensamos en los efectos
+
+llamamos una api como ejemplo
 
 */
---
+++ react-hooks/src/components/Characters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Characters = () => {
+    return (
+
+    );
+}
+
+export default Characters;
