9c32df0b886d17a0cfd5a6e4b085db034c989ae7
alejandro zeballos aguilar <alejandrojzeballos@gmail.com>
2022-03-30 21:15:12 -0400

    {add} configurando babel y añadiendo scripts

--- platzi-conf-merch/webpack.config.js
+++ platzi-conf-merch/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -36,7 +37,7 @@ module.exports = {
         }),
     ],
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        static: path.join(__dirname, 'dist'),
         compress: true,
         port: 3005,
     }
