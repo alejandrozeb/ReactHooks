ca6f879647da9f2108f03d3c7b9193df2f9b12dc
alejandro zeballos aguilar <alejandrojzeballos@gmail.com>
2022-04-04 21:03:31 -0400

    {add} añadediendo las rutas a la configuracion

--- platzi-conf-merch/src/routes/App.jsx
+++ platzi-conf-merch/src/routes/App.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../containers/Home';
+import Checkout from '../containers/CheckOut';
+import Information from '../containers/Information';
+import Payment from '../containers/Payment';
+import Success from '../containers/Success';
+import NotFound from '../containers/NotFound';
 
 const App = () => {
     return (
         <BrowserRouter>
-        <Switch>
-            <Router path="/" component={Home} />
-            <Router path="/checkout" component={Checkout} />
-            <Router path="/checkout/information" component={Information} />
-            <Router path="/checkout/payment" component={Payment} />
-            <Router path="/checkout/success" component={Success} />
-            <Router component={} />
-        </Switch>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/checkout" element={<Checkout/>} />
+            <Route path="/checkout/information" element={<Information/>} />
+            <Route path="/checkout/payment" element={<Payment/>} />
+            <Route path="/checkout/success" element={<Success/>} />
+            <Route path='*' element={<NotFound/>} />
+
+        </Routes>
         </BrowserRouter>
     );
 }
