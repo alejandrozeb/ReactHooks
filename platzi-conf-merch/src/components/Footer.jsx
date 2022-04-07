3c5f182009275019c2bb36fb781f5ad3080ae62d
alejandro zeballos aguilar <alejandrojzeballos@gmail.com>
2022-04-06 20:35:50 -0400

    {add} añadiendo estilos a footer, header y layout

---
+++ platzi-conf-merch/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../components/Footer.css';
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <p className="Footter-title">
+                Platzi Conf Merch
+            </p>
+            <p className="Footer-copy">
+                Todos los derechos reservados
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
--- platzi-conf-merch/src/components/Layout.jsx
+++ platzi-conf-merch/src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React, {Children} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import '../styles/components/Layout.css'
 
 const Layout = ({Children}) => {
     return (
