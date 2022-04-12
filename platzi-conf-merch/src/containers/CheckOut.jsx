f72626683b299a6c9e8371c753f9a5d28a92c4a3
alejandro zeballos aguilar <alejandrojzeballos@gmail.com>
2022-04-11 23:08:49 -0400

    {add} añadiendo el container Checkout

--- platzi-conf-merch/src/containers/CheckOut.jsx
+++ platzi-conf-merch/src/containers/CheckOut.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import '../styles/components/Checkout.css';
 
 const CheckOut = () => {
-  return <h1>CheckOut</h1>;
+  return (
+    <div className="Checkout">
+      <div className="Checkout">
+        <h3>Lista Pedidos:</h3>
+        <div className="Checkout-item">
+          <div className="checkout-element">
+            <h4>ITEM name</h4>
+            <span>$10</span>
+          </div>
+          <button type='button'>Eliminar</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Precio Total: $10</h3>
+        <button type='button'>Continuar pedido</button>
+      </div>
+    </div>
+  );
 };
 
 export default CheckOut;
