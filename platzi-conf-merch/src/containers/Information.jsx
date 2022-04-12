6fae4da809d2619fd149af31cdac2e7a305be348
alejandro zeballos aguilar <alejandrojzeballos@gmail.com>
2022-04-12 19:54:42 -0400

    {add} añadiendo maquetacion del container Information

--- platzi-conf-merch/src/containers/Information.jsx
+++ platzi-conf-merch/src/containers/Information.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React from 'react';
+import '../styles/components/Information.css';
 
 const Information = () => {
-    return (
-        <h1>Information</h1>
-    );
+  return (
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Informacion de contacto</h2>
+        </div>
+        <div className="Information-form">
+          <form action="">
+            <input type="text" placeholder="Nombre completo" name="name" />
+            <input type="text" placeholder="Correo Electronico" name="email" />
+            <input type="text" placeholder="Direccion" name="addres" />
+            <input type="text" placeholder="Apto" name="apto" />
+            <input type="text" placeholder="Ciudad" name="city" />
+            <input type="text" placeholder="Pais" name="Country" />
+            <input type="text" placeholder="Estado" name="state" />
+            <input type="text" placeholder="Codigo postal" name="cp" />
+            <input type="text" placeholder="Telefono" name="phone" />
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">Regresar</div>
+          <div className="Information-next">pagar</div>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Pedido: </h3>
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>ITEM name</h4>
+            <span>$</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Information;
