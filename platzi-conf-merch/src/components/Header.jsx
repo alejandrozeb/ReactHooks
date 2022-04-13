dc3682927b1a6aadf25a2ae670a34ce637233f76
alejandro zeballos aguilar <alejandrojzeballos@gmail.com>
2022-04-12 22:44:27 -0400

    {add} añadeindo link al header

--- platzi-conf-merch/src/components/Header.jsx
+++ platzi-conf-merch/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Header.css';
 
 const Header = () => {
   return (
     <div className="Header">
       <h1 className="Header-title">PlatziConf Merch</h1>
-      <div className="Header-checkout">checkoutasdasd</div>
+      <div className="Header-checkout">
+        <Link to="/checkout">Checkout</Link>
+      </div>
     </div>
   );
 };
